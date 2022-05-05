9b9131ad6e66fa3114aff2826070ad93d84d4d1d
dicapisar <dicapisar@gmail.com>
2022-05-04 22:42:28 -0500

    Initialization Proyect

---
+++ src/main/java/com/dicapisar/outputItemManager/OutputItemManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.dicapisar.outputItemManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutputItemManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutputItemManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dicapisar/outputItemManager/OutputItemManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dicapisar.outputItemManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutputItemManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
